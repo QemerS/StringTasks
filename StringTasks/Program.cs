5b47f696a71a2958efbf2248d161dc8c5d1e2f7a
QemerS <tu201806169@code.edu.az>
2021-11-02 05:38:13 +0400

    Add project files.

---
+++ StringTasks/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace StringTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task1
+            //Console.WriteLine("stringi daxil edin"); string str = Console.ReadLine();
+            //Console.WriteLine("The string you entered is:"+str);
+            #endregion
+
+            #region Task2
+            //Console.WriteLine("stringi daxil edin"); string str = Console.ReadLine();
+            //int length = str.Length;
+            //Console.WriteLine("Length of the string is:"+length);
+            #endregion
+
+            #region Task3
+            //Console.WriteLine("stringi daxil edin"); string str = Console.ReadLine();
+            //string[] array = str.Split();
+            //Array.Reverse(array);
+            //Console.Write("The caracters of the string are:");
+            //for (int i = 0;i<array.Length; i++)
+            //{
+            //    Console.Write(array[i]+" ");
+            //}
+            #endregion
+
+            #region Task5
+            //Console.WriteLine("stringi daxil edin"); string str = Console.ReadLine();
+            //byte times = 0;
+           
+            
+            //while (str.Contains("the"))
+            //{
+            //    times++;
+            //    }
+            //Console.WriteLine("The frequency of the word 'the' is: "+times);
+            #endregion
+        }
+    }
+}
